7252bd30491234ad438754cd310266b9c7ccff53
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-10-26 23:30:52 +0300

    added constructor, getters, setters

--- src/plane/plane.hpp
+++ src/plane/plane.hpp
@@ -1,8 +1,22 @@
 #include <string>
 
 class Plane {
+    public:
+        Plane(std::string model, int capacity, double maxSpeed, double fuelCapacity, double maxAltitute);
+        std::string getModel();
+        int getCapacity();
+        double getMaxSpeed();
+        double getFuelCapacity();
+        double getMaxAltitute();
+        void setModel(std::string model);
+        void setCapacity(int capacity);
+        void setMaxSpeed(double maxSpeed);
+        void setFuelCapacity(double fuelCapacity);
+        void setMaxAltitute(double maxAltitute);
     private:
         std::string model;
         int capacity;
         double maxSpeed;
+        double fuelCapacity;
+        double maxAltitute;
 };
